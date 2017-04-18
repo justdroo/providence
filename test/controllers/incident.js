bb90ca97ffe9304bc1ad0ba2ad54a1c9547a85cb
Drew Nickerson <dnick13@gmail.com>
2017-04-18 14:58:47 -0400

    Get and GetAll testing complete

--- test/controllers/incident.js
+++ test/controllers/incident.js
@@ -17,7 +17,7 @@ require('dotenv-safe').load();
 //
 // Models
 //
-let Acronym = require('../../app/models/incident');
+let Incident = require('../../app/models/incident');
 
 //
 // Server
@@ -60,14 +60,146 @@ before((done) => {
 // Test Parameters
 // ====================
 
+let testIncidents = [
+  {
+    date: "1995-08-02T17:03:13.441Z",
+    location: {
+      city: "Little Winging",
+      state: "Surrey",
+      zip: 77777,
+      category: "home"
+    },
+    context: {
+      category: "dementor attack",
+      reason: "muggle born",
+      description: "Dementors attacked a muggle and a wizard"
+    },
+    victim: {
+      name: "Dudley Dursley",
+      anonymous: false
+    }
+  }, {
+    date: "1999-05-02T17:03:13.441Z",
+    location: {
+      city: "Hogwarts Castle",
+      state: "Scotland",
+      zip: 77777,
+      category: "school"
+    },
+    context: {
+      category: "death eater attack",
+      reason: "blood status",
+      description: "Voldemort and his death eaters attacked the castle"
+    },
+    victim: {
+      name: "Student Body",
+      anonymous: true
+    }
+  }, {
+    date: "1995-08-02T17:03:13.441Z",
+    location: {
+      city: "Dartmoor",
+      state: "England",
+      zip: 77777,
+      category: "sporting event"
+    },
+    context: {
+      category: "death eater attack",
+      reason: "muggle born",
+      description: "Death eaters attacked and sent up the dark mark at the Quidditch World Cup"
+    },
+    victim: {
+      name: "Robert's Family",
+      anonymous: false
+    }
+  }
+]
 
 // ====================
 // Testing
 // ====================
-describe('#getAll', function () {
-  it('should return all incidents in the database', function(done){
-    var sum = 1 + 1;
-    sum.should.equal(2);
-    done();
-  })
-})
+
+//
+// Reset incident in database
+//
+beforeEach(done => {
+  Incident.remove({}, () => {
+    testIncidents.forEach((incident, index) => {
+      Incident.collection.insert(incident, () => {
+        if (index === testIncidents.length - 1) done();
+      })
+    });
+  });
+});
+
+//
+// #GET /incident
+//
+
+describe('GET /incident (#getAll)', () => {
+  it('should return status 200', (done) => {
+    chai.request(address)
+      .get('/incident')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.success.should.equal(true);
+        done();
+      });
+  });
+
+  it('should return a JSON object with "count" and "incidents"', (done) => {
+    chai.request(address)
+      .get('/incident')
+      .end((err, res) => {
+        res.body.should.have.property('count');
+        res.body.should.have.property('incidents');
+        done();
+      })
+  });
+
+  it('should return all incidents as an array', (done) =>{
+    chai.request(address)
+      .get('/incident')
+      .end((err, res) => {
+        res.body.incidents.should.be.a('array');
+        res.body.incidents.should.have.length(testIncidents.length);
+        done();
+      })
+  });
+});
+
+//
+// #GET /incident/:id
+//
+
+describe('GET /incident/:id (#get)', () => {
+  it('should return a status code 200', (done) => {
+    Incident.find({}, (err, incidents) => {
+      let incident = incidents[0];
+
+      chai.request(address)
+        .get(`/incident/${incident._id}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.success.should.equal(true)
+          done();
+        });
+    });
+  });
+
+  it('should return a JSON object with an incident array of one object', (done) => {
+    Incident.find({}, (err, incidents) => {
+      let incident = incidents[0];
+
+      chai.request(address)
+        .get(`/incident/${incident._id}`)
+        .end((err, res) => {
+          res.body.should.be.a('object');
+          res.body.should.have.property('incident')
+          res.body.incident.should.have.length(1)
+          res.body.incident[0].should.be.a('object')
+          done();
+      });
+    });
+  });
+});
