c1ab5e1214c3fcc62f48ac05d946598d0912169c
Drew Nickerson <dnick13@gmail.com>
2017-04-18 11:37:08 -0400

    Adding testing

---
+++ test/controllers/incident.js
@@ -0,0 +1,57 @@
+// ====================
+// Third-Party Modules
+// ====================
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let mongoose = require('mongoose');
+let Mockgoose = require('mockgoose').Mockgoose;
+
+require('dotenv-safe').load();
+
+
+// ====================
+// Internal Modules
+// ====================
+
+//
+// Models
+//
+let Acronym = require('../../app/models/incident');
+
+//
+// Server
+//
+let server = require('../../server');
+
+// ====================
+// Setup
+// ====================
+
+//
+// Assertions
+//
+let should = chai.should();
+
+//
+// Port
+//
+let port = process.env.PORT;
+let address = `http://localhost:${port}`;
+
+//
+// Chai HTTP
+//
+chai.use(chaiHttp);
+
+//
+// Mockgoose (Test Database Connection)
+//
+let mockgoose = new Mockgoose(mongoose);
+before((done) => {
+  mockgoose.prepareStorage().then(() => {
+    mongoose.createConnection(process.env.MONGODB_URI_TEST, (err) => {
+      done(err);
+    });
+  });
+});
